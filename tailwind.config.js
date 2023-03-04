a7d2d0d31ab348e23e1f7bab442701f45e024f77
OnderCataltepe <ondercataltepe@gmail.com>
2023-03-04 13:48:36 +0300

    init2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  darkMode: 'class',
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      keyframes: {
+        rToL: {
+          from: { right: '-30px' },
+          to: { right: '0px' }
+        }
+      },
+      animation: {
+        iconText3: 'rToL 0.5s linear'
+      }
+    }
   },
   plugins: []
 };
